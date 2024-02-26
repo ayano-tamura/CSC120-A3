3f2ad1399d5dbbe5382cc209a35fa42924bbb920
ayano <atamura@smith.edu>
2024-02-25 19:51:18 -0500

    Assignment Completed

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,96 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Conversation class creates a chatbot that interacts with user input.
+ * Users can specify the number of rounds to have the chat, and the chatbot will return pre-planned phrases or convert terms of certain phrases from the user's input and return them to the user as a response.
+ */
 class Conversation {
+/**
+ * Method that starts conversation with the chatbot.
+ * @param arguments
+ */
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random() ;
+    List<String> transcript = new ArrayList<>();
+    
+    // Intro for chatbot
+    System.out.println("Hello! Welcome to the chatbot!");
+    transcript.add("Hello! Welcome to the chatbot!");
+    System.out.println("How many rounds of conversation would you like to have?");
+    transcript.add("How many rounds of conversation would you like to have?");
+
+    // Ask for number of rounds
+    int rounds = scanner.nextInt();
+    scanner.nextLine();
+    transcript.add(Integer.toString(rounds));
+    
+    // Start conversation
+    System.out.println("Hi there! What's on your mind?");
+    transcript.add("Hi there! What's on your mind?");
+
+    int interaction = 0;
+
+    // Conversation loop for chatbot to take in and return responses
+    while (rounds > 0) {
+      String response = scanner.nextLine();
+      transcript.add(response);
+    
+      if (response.toLowerCase().contains("\\bi\\b")||
+      response.toLowerCase().contains("me")||
+      response.toLowerCase().contains("am")||
+      response.toLowerCase().contains("you")||
+      response.toLowerCase().contains("my")||
+      response.toLowerCase().contains("your")) {
+        response = response.replaceFirst ("\\bi\\b", "temp");
+        response = response.replaceFirst ("\\bme\\b","temp");
+        response = response.replaceFirst ("\\bam\\b","are");
+        response = response.replaceFirst ("\\byou\\b","I");
+        response = response.replaceFirst ("\\bmy\\b","your");
+        response = response.replaceFirst ("\\byour\\b","my");
+        response = response.replaceFirst ("\\btemp\\b","you");
+        System.out.println(response + "?");
+        transcript.add(response + "?");
+        interaction ++;
+      }
+      
+      // System.out.println(response + "?");
+      //   transcript.add(response + "?");
+      else {
+        // Return pre-planned responses
+        String [] comments = {
+          "Mmm-hm.",
+          "Tell me more about it!",
+          "Interesting!"
+        };
+        int generator = random.nextInt(comments.length);
+        String generated = comments[generator];
+        System.out.println(generated); 
+        transcript.add(generated);
+        interaction ++;
+      } 
+
+      // Check if rounds of interactions specified is reached
+      if (interaction == rounds) {
+        break;
+      }
+    }
+    // Close chatbot
+    System.out.println("Great talking to you, see ya!");
+    transcript.add("Great talking to you, see ya!");
+    System.out.println("");
+    System.out.println("TRANSCRIPT");
+    System.out.println("----------------------------------------");
+    // Display full conversation in a transcript
+    for (String printout : transcript) {
+      System.out.println(printout);
+    }
+    System.out.println("---------------------------------------");
+    scanner.close();
+  
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+Sakura & Tulip
 - Which session(s) of TA / office hours did you attend?
+Feb 13 and Feb 20
 - What are your initial impressions of Java? 
+It was a mixed feeling between "it was easier than I thought" and "this is very different from Python". Learning a new language is difficult, but I was able to figure out the basic steps and attributes, classes, etc. easier than I thought. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+I thought it was interesting how Python takes indentation into account when nesting different variables, whereas indentation can be used in Java for clarity purposes only and is not necessary. Java seems to require more specific information in terms of which information will be made public and private. In python, integers and strings for example do not need to be specified but in Java, you would have to specify which one you will be using in the code prior to printing it out. Additionally, since Python runs line by line, the code runs up until the error code. However, for Java, the code as a whole does not run unless you fix all bugs in the code. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I had some trouble implementing the if statements into the code, with replacing the words. I had similar difficulty in Python in separating words, but I was able to figure it out, and my TA also taught me to put all of the conditions in one if statements. I am curious to find out how to change the code so the bot accounts for more word replacements.
